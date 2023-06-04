0e92f1552733de21e97be824687e5dd4a8f2abe2
Endri Muço <endrimuco19@gmail.com>
2023-06-04 18:42:13 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [newItem, setNewItem] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <form className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input type="text" id="item"/>
     </div>
-  );
-}
+    <button className="btn">Add</button>
 
-export default App;
+  </form>
+  <h1 className="header">Todo List</h1>
+  <ul className="list">
+    <li>
+      <label>
+        <input type="checkbox"/>
+        Item 1
+      </label>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox"/>
+        Item 2
+      </label>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+  </ul>
+
+  </>
+  
+  )
+}
